74fd88b0194b07affe926653ca473491e3785dfa
Will Prendergast <prendygast@hotmail.com>
2020-02-09 21:17:56 +0000

    moving onto 'create your urls'

--- blogapp/models.py
+++ blogapp/models.py
@@ -10,8 +10,8 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     publish_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     views = models.IntegerField(default=0)
-    tag = models.Charfield(max_length=30, blank=True, null=True)
-    image = models.Imagefield(upload_to="img", blank=True, null=True)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
 
     def __unicode__(self):
         return self.title
